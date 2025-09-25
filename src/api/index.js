03f212ef3d6a8eebee3c031412cfd91d6b367ffd
heatherd2025 <heatherdeliso@gmail.com>
2025-09-25 15:30:55 -0400

    base api revisions

--- src/api/index.js
+++ src/api/index.js
@@ -1,26 +1,10 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
-import { getToken } from "/src/utils/tokenService.js";
+import { getToken } from "../utils/tokenService.js";
 
 const BASE_URL = import.meta.env.VITE_BACKEND_URL || "http://localhost:5000";
 
-// const api = createApi({
-//   baseQuery: fetchBaseQuery({
-//     baseUrl: `${BASE_URL}/api/`,
-//     prepareHeaders: (headers) => {
-//       const LOCAL_TOKEN = getToken();
-//       if (LOCAL_TOKEN) {
-//         headers.set("authorization", `Bearer ${LOCAL_TOKEN}`);
-//       }
-//       return headers;
-//     },
-//   }),
-//   tagTypes: ["User"],
-//   endpoints: () => ({}),
-// });
-
-// export default api;
 export const baseQuery = fetchBaseQuery({
-  baseUrl: `${BASE_URL}`, // DOUBLE CHECK THIS
+  baseUrl: BASE_URL, // DOUBLE CHECK THIS
   prepareHeaders: (headers) => {
     const LOCAL_TOKEN = getToken();
     if (LOCAL_TOKEN) {
