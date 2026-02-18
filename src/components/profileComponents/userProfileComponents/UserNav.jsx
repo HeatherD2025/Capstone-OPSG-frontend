20d7d3906502a83e77738bd8593c28f161c3da2d
heatherd2025 <heatherdeliso@gmail.com>
2026-02-18 15:22:12 -0500

    added missing useEffer import

--- src/components/profileComponents/userProfileComponents/UserNav.jsx
+++ src/components/profileComponents/userProfileComponents/UserNav.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import { Button } from "react-bootstrap";
 import { useDispatch } from "react-redux";
