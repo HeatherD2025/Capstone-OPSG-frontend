8435b6df80e514459b519aad21608862ea7ad254
heatherd2025 <heatherdeliso@gmail.com>
2026-03-05 20:45:03 -0500

    fixed useState and useSelector error

--- src/components/profileComponents/DashTabs.jsx
+++ src/components/profileComponents/DashTabs.jsx
@@ -1,43 +1,49 @@
-import Button from 'react-bootstrap/Button';
-import Card from 'react-bootstrap/Card';
-import Nav from 'react-bootstrap/Nav';
+import Button from "react-bootstrap/Button";
+import Card from "react-bootstrap/Card";
+import Nav from "react-bootstrap/Nav";
 import Balance from "../qbComponentsAndHooks/Balance";
 import { BalanceProvider } from "./BalanceProvider";
-import { useState } from 'react';
+import { useState } from "react";
 import { useSelector } from "react-redux";
 
-const [firstTab, setFirstTab] = useState(true);
-const { isAdmin, user } = useSelector((state) => state.auth)
-
 function DashTabs() {
+  const [firstTab, setFirstTab] = useState(true);
+  const { isAdmin, user } = useSelector((state) => state.auth);
+
   return (
     <Card>
       <Card.Header>
         <Nav variant="tabs" defaultActiveKey="#first">
           <Nav.Item>
-            <Nav.Link href="#first" onClick={() => setFirstTab(true)}>Balance</Nav.Link>
+            <Nav.Link href="#first" onClick={() => setFirstTab(true)}>
+              Balance
+            </Nav.Link>
           </Nav.Item>
           <Nav.Item>
-            <Nav.Link href="#second"onClick={() => setFirstTab(false)}>Quickbooks</Nav.Link>
+            <Nav.Link href="#second" onClick={() => setFirstTab(false)}>
+              Quickbooks
+            </Nav.Link>
           </Nav.Item>
         </Nav>
-
       </Card.Header>
       <Card.Body>
         <Card.Title>
-         {!isAdmin && user && firstTab ? 
-            <BalanceProvider id={user?.id}> 
-                 <Balance /> 
+          {!isAdmin && user && firstTab ? (
+            <BalanceProvider id={user?.id}>
+              <Balance />
             </BalanceProvider>
-            :  ""}
+          ) : (
+            ""
+          )}
         </Card.Title>
         <Card.Text>
           {!isAdmin && user && !firstTab ? "Connect to Quickbooks" : ""}
         </Card.Text>
-          {!isAdmin && user && !firstTab ? 
-            <Button>Connect</Button> : 
-            <Button>View Invoice</Button>}
-
+        {!isAdmin && user && !firstTab ? (
+          <Button>Connect</Button>
+        ) : (
+          <Button>View Invoice</Button>
+        )}
       </Card.Body>
     </Card>
   );
