a56bc73b26e261b272a21a8d9f553ff7b173bf92
heatherd2025 <heatherdeliso@gmail.com>
2026-03-30 16:04:51 -0400

    added loading state to adminRoute

--- src/routes/AdminRoute.jsx
+++ src/routes/AdminRoute.jsx
@@ -2,8 +2,12 @@ import { Navigate } from "react-router-dom";
 import { useSelector } from "react-redux";
 
 export default function AdminRoute({ children }) {
-  const { isAuthenticated, isAdmin } = useSelector((state) => state.auth);
+  const { isAuthenticated, isAdmin, isAuthLoading } = useSelector((state) => state.auth);
 
+  if (isAuthLoading) {
+    return <p>Loading...</p>;
+  }
+  
   if (!isAuthenticated || !isAdmin) {
     return <Navigate to="/" replace />;
   }
